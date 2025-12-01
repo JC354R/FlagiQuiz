825b4dcb3b80fd6a389cf50b288b0175ef2d1f6e
JC354R <kopertowska.julia@gmail.com>
2025-12-01 15:22:33 +0100

    Koniec

--- app/src/main/java/com/example/flagi/MainActivity.java
+++ app/src/main/java/com/example/flagi/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int licznik = 0;
+    TextView textViewPolecenie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPolecenie = findViewById(R.id.textViewPolecenie);
     }
 
     public void poprawnaOdpowiedz(View view) {
@@ -21,5 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public void zlaOdpowiedzUkryj(View view) {
         //Toast.makeText(this, "Ten przycisk jest poprawny", Toast.LENGTH_SHORT).show();
         view.setVisibility(view.INVISIBLE);
+        licznik++;
+        if(licznik == 4){
+            Toast.makeText(this, "Brawo to jest flaga polski!!!", Toast.LENGTH_SHORT).show();
+            textViewPolecenie.setText(R.string.gratulacje);
+        }
     }
 }
