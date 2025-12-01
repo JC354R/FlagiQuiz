7bed57eae539eaf75ff6805977b80ce2f3d2ff0c
JC354R <kopertowska.julia@gmail.com>
2025-12-01 15:13:49 +0100

    Akcje Guzikow

--- app/src/main/java/com/example/flagi/MainActivity.java
+++ app/src/main/java/com/example/flagi/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flagi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void poprawnaOdpowiedz(View view) {
+        Toast.makeText(this, R.string.ostrzezenie, Toast.LENGTH_SHORT).show();
+    }
+
+    public void zlaOdpowiedzUkryj(View view) {
+        //Toast.makeText(this, "Ten przycisk jest poprawny", Toast.LENGTH_SHORT).show();
+        view.setVisibility(view.INVISIBLE);
     }
 }
